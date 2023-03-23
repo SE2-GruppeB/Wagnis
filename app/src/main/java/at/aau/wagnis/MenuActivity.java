6d5d3489374ee12c3d99329db5802fedbb9a4d98
greenkranz <kevingr@edu.aau.at>
2023-03-23 23:41:50 +0100

    Added Basic Sources popup

--- app/src/main/java/at/aau/wagnis/MenuActivity.java
+++ app/src/main/java/at/aau/wagnis/MenuActivity.java
@@ -14,12 +14,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
     Button start;
+    Button sources;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         hideNavigationBar();
 
+        sources = findViewById(R.id.btn_sources);
+        sources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSources(sources);
+
+            }
+        });
         start = findViewById(R.id.btn_start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +48,30 @@ public class MenuActivity extends AppCompatActivity {
         Intent switchActivityIntent = new Intent(this, MainActivity.class);
         startActivity(switchActivityIntent);
     }
+    public void showSources(View view) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUp = inflater.inflate(R.layout.popup_sources, null);
+
+
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 240, getResources().getDisplayMetrics());
+        int width = (int)px;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = false; // lets taps outside the popup also dismiss it
+        PopupWindow popupWindow = new PopupWindow(popUp, width, height, focusable);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+
+        Button btnClose = popUp.findViewById(R.id.btn_Close);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+                return;
+            }
+        });
+    }
+
 
     public void chooseFighterPopUp(View view) {
 

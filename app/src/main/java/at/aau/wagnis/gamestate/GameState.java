9ec5942ab68e3dd5d84186350991062d0660222b
martinpirker <126809496+martinpirker@users.noreply.github.com>
2023-04-11 01:24:21 +0200

    Creation of the classes GameState, GameLogicState,MoveTroopsState and Player in the state pattern style. GameState gets his own package.

--- app/src/main/java/at/aau/wagnis/gamestate/GameState.java
+++ app/src/main/java/at/aau/wagnis/gamestate/GameState.java
@@ -5,6 +5,7 @@ import java.util.List;
 import at.aau.wagnis.Hub;
 import at.aau.wagnis.Player;
 
+
 public class GameState {
     private final List<Hub> hubs;
     private final List<Player> players;

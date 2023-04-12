8d8a331099b09d287679769bb887b289665b5927
mwalkam <126983544+mwalkam@users.noreply.github.com>
2023-04-12 21:28:18 +0200

    Add connection demo

--- app/src/main/java/at/aau/wagnis/MenuActivity.java
+++ app/src/main/java/at/aau/wagnis/MenuActivity.java
@@ -2,6 +2,7 @@ package at.aau.wagnis;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -12,31 +13,65 @@ import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import at.aau.wagnis.server.communication.command.ProcessChatMessageCommand;
+import at.aau.wagnis.server.communication.connection.ClientConnectionBus;
+import at.aau.wagnis.server.communication.connection.ClientConnectionBusImpl;
+import at.aau.wagnis.server.communication.connection.ClientConnectionListener;
+import at.aau.wagnis.server.communication.connection.NetworkClientConnection;
+import at.aau.wagnis.server.communication.connection.NetworkServerConnection;
+
 public class MenuActivity extends AppCompatActivity {
-    Button start;
-    Button sources;
+
+    private static final int DEMO_PORT = 54321;
+    private static final String EMULATOR_HOST_ADDRESS = "10.0.2.2";
+    private static final String DEMO_SERVER_TAG = "DemoServer";
+    private static final String DEMO_CLIENT_TAG = "DemoClient";
+
+    Button hostBtn;
+    Button sourcesBtn;
+    Button joinBtn;
+
+    Thread demoThread = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         hideNavigationBar();
 
-        sources = findViewById(R.id.btn_sources);
-        sources.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSources(sources);
+        sourcesBtn = findViewById(R.id.btn_sources);
+        sourcesBtn.setOnClickListener(view -> showSources(sourcesBtn));
 
+        hostBtn = findViewById(R.id.btn_start);
+        hostBtn.setOnClickListener(view -> {
+            if (demoThread == null) {
+                demoThread = new Thread(this::createNetworkingDemoServer);
+                demoThread.start();
+            }
+
+            chooseFighterPopUp(hostBtn);
+        });
+
+        joinBtn = findViewById(R.id.btn_join);
+        joinBtn.setOnClickListener(view -> {
+            if (demoThread == null) {
+                demoThread = new Thread(this::createNetworkingDemoClient);
+                demoThread.start();
             }
         });
-        start = findViewById(R.id.btn_start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chooseFighterPopUp(start);
+    }
 
-            }
-        });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (demoThread != null) {
+            demoThread.interrupt();
+        }
     }
 
     public void hideNavigationBar(){
@@ -96,5 +131,59 @@ public class MenuActivity extends AppCompatActivity {
                 return;
             }
         });
+    }
+
+    private void createNetworkingDemoClient() {
+        Log.i(DEMO_CLIENT_TAG, "Connecting...");
+        try (Socket clientSocket = new Socket(EMULATOR_HOST_ADDRESS, DEMO_PORT)) {
+            Log.i(DEMO_CLIENT_TAG, "Connected");
+
+            NetworkServerConnection serverConnection = NetworkServerConnection.fromSocket(
+                    clientSocket,
+                    Thread::new,
+                    command -> Log.i(DEMO_CLIENT_TAG, command.toString())
+            );
+
+            serverConnection.start();
+
+            while(!Thread.interrupted()) {
+                serverConnection.send(new ProcessChatMessageCommand("Hello from client"));
+                //noinspection BusyWait
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            Log.e(DEMO_CLIENT_TAG, "IOException while running demo client", e);
+        }
+    }
+
+    private void createNetworkingDemoServer() {
+        Log.i(DEMO_SERVER_TAG, "Starting demo server");
+        ClientConnectionBus bus = new ClientConnectionBusImpl();
+
+        try (ServerSocket serverSocket = new ServerSocket(DEMO_PORT)) {
+            ClientConnectionListener listener = new ClientConnectionListener(
+                    serverSocket,
+                    bus,
+                    NetworkClientConnection::fromSocket,
+                    Thread::new
+            );
+            listener.start();
+
+            while (!Thread.interrupted()) {
+                Log.i(DEMO_SERVER_TAG, bus.getNextCommand().toString());
+                bus.broadcastCommand(new ProcessChatMessageCommand("Hello from Server"));
+            }
+
+        } catch (IOException e) {
+            Log.e(DEMO_SERVER_TAG, "IOException while running demo server", e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            bus.close();
+        }
+
+        Log.i(DEMO_SERVER_TAG, "Closed demo server");
     }
 }

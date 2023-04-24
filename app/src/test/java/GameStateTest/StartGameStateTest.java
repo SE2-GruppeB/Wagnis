8c91175023184817b565e475645471cde592cce7
TWR-Dominik <Dominik.hobel2001@gmail.com>
2023-04-24 11:02:28 +0200

    added Tests for GameStates

---
+++ app/src/test/java/GameStateTest/StartGameStateTest.java
@@ -0,0 +1,35 @@
+package GameStateTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static at.aau.wagnis.GlobalVariables.players;
+
+import android.widget.Button;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import at.aau.wagnis.Hub;
+import at.aau.wagnis.Player;
+import at.aau.wagnis.gamestate.StartGameState;
+
+public class StartGameStateTest {
+
+    @Before
+    public void setUp() {
+
+
+
+    }
+
+    @Test
+    public void testAssignCountries() {
+
+
+    }
+}

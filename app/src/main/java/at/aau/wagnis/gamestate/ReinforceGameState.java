a1e9731e03c841d05b2963f0ef613b8a8ffc4322
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2023-04-28 14:26:18 +0200

    Update ReinforceGameState.java
    Class ReinforceGameState was updated after discussion with colleague
    
    #25

--- app/src/main/java/at/aau/wagnis/gamestate/ReinforceGameState.java
+++ app/src/main/java/at/aau/wagnis/gamestate/ReinforceGameState.java
@@ -1,32 +1,40 @@
 package at.aau.wagnis.gamestate;
 
+import java.util.List;
+
 import at.aau.wagnis.Hub;
 import at.aau.wagnis.Player;
 
 public class ReinforceGameState{
-    private Player player;
+    private List<Hub> hubs;
+    private List<Integer> troopsToDeploy;
 
-
-    public ReinforceGameState(Player player) {
-        this.player = player;
+    public ReinforceGameState(List<Hub> hubs, List<Integer> troopsToDeploy) {
+        this.hubs = hubs;
+        this.troopsToDeploy = troopsToDeploy;
     }
 
-    public Player getPlayer() {
-        return player;
+    public List<Hub> getHubs() {
+        return hubs;
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
+    public void setHubs(List<Hub> hubs) {
+        this.hubs = hubs;
     }
 
-    public Hub playerChosenHub() {
-        //TODO implement this
-        return null;
+    public List<Integer> getTroopsToDeploy() {
+        return troopsToDeploy;
     }
+
+    public void setTroopsToDeploy(List<Integer> troopsToDeploy) {
+        this.troopsToDeploy = troopsToDeploy;
+    }
+
+
 
     public void reinforce() {
         //TODO implement this
 
-        
+
     }
 }

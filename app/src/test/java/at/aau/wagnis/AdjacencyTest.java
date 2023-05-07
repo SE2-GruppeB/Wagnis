891f4b3a7d7f5fc120d0c0e69c1ed59ad5a8322a
jplanegger <joplanegger@edu.aau.at>
2023-05-07 14:59:01 +0200

    added test for Adjacency

---
+++ app/src/test/java/at/aau/wagnis/AdjacencyTest.java
@@ -0,0 +1,41 @@
+package at.aau.wagnis;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class AdjacencyTest {
+
+    @Mock Hub hub1;
+    @Mock Hub hub2;
+
+    Adjacency adjacency;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        when(hub1.toString()).thenReturn("hub1");
+        when(hub2.toString()).thenReturn("hub2");
+
+        adjacency = new Adjacency(hub1, hub2);
+    }
+
+    @Test
+    void getHub1() {
+        assertSame(hub1, adjacency.getHub1());
+        assertNotSame(hub2, adjacency.getHub1());
+    }
+
+    @Test
+    void getHub2() {
+        assertSame(hub2, adjacency.getHub2());
+        assertNotSame(hub1, adjacency.getHub2());
+    }
+}

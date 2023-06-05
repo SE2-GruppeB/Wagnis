aed9a2326c516345d2b3d6595319f44b9e39a828
martinpirker <126809496+martinpirker@users.noreply.github.com>
2023-06-05 13:03:46 +0200

    In der ChooseAttackGameState auf die Adjacency von Hubs prüfen.

--- app/src/main/java/at/aau/wagnis/gamestate/ChooseAttackGameState.java
+++ app/src/main/java/at/aau/wagnis/gamestate/ChooseAttackGameState.java
@@ -1,9 +1,33 @@
 package at.aau.wagnis.gamestate;
 
+import static at.aau.wagnis.GlobalVariables.adjacencies;
+
+import at.aau.wagnis.Adjacency;
+import at.aau.wagnis.Hub;
+import at.aau.wagnis.Player;
+
 public class ChooseAttackGameState extends GameLogicState {
+    private boolean areHubsAdjacent(Hub sourceHub, Hub targetHub) {
+    for (Adjacency adjacency : adjacencies) {
+        if ((adjacency.getHub1() == sourceHub && adjacency.getHub2() == targetHub) ||
+                (adjacency.getHub1() == targetHub && adjacency.getHub2() == sourceHub)) {
+            return true;
+        }
+    }
+    return false;
+}
     @Override
     public void chooseAttack(int playerId, int sourceHubId, int targetHubId) {
-        this.gameServer.getGameData();
-        this.gameServer.setGameLogicState(new AttackGameState(sourceHubId, targetHubId));
+        Hub sHub = new Hub(targetHubId);
+        Hub tHub = new Hub(targetHubId);
+        if(areHubsAdjacent(sHub, tHub) == true) {
+            this.gameServer.getGameData();
+            this.gameServer.setGameLogicState(new AttackGameState(sourceHubId, targetHubId));
+            //Erstellt zwei neue Hubs, mit übergebner ID von Source und Target prüft auf adjacency
+        }
+        else{
+            throw new IllegalArgumentException("Nur angrenzende Hubs können Angegriffen werden!");
+        }
     }
+
 }

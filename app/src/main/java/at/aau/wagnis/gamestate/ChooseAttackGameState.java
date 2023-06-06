4c9348cdf9dd8d1a3f58367ea62ecf2f0e959c5b
martinpirker <126809496+martinpirker@users.noreply.github.com>
2023-06-06 19:42:32 +0200

    Update ChooseAttackGameState.java
    Verwendung der isInPair im ChooseAttackGameState und throw IllegalArgumentException

--- app/src/main/java/at/aau/wagnis/gamestate/ChooseAttackGameState.java
+++ app/src/main/java/at/aau/wagnis/gamestate/ChooseAttackGameState.java
@@ -16,26 +16,29 @@ public class ChooseAttackGameState extends GameLogicState {
         List<Hub> hubs = this.gameServer.getGameData().getHubs();
         // Liste aller Hubs im Spiel
         List<Adjacency> adj = this.gameServer.getGameData().getAdjacencies();
-        //Liste aller benachbarten Hubpaare
+        // Liste aller benachbarten Hubpaare
 
         // Durchlaufen aller Knoten
-        for(Hub h :hubs){
+        for(Hub h : hubs){
             if(h.getId()==sourceHubId){
                 sourceHub=h;
                 // Findet den Hub welcher übereinstimmt und weise ihn sourceHub zu
             }
-            if(h.getId()==sourceHubId){
-                sourceHub=h;
+            if(h.getId()==targetHubId){
+                targetHub=h;
                 // Findet den Hub welcher übereinstimmt und weise ihn targetHub zu
             }
         }
         // Durchlaufe alle benachbarten Hubpaare in der Liste
         for(Adjacency a : adj){
             // Überprüfe, ob das aktuelle Hubpaar entweder den sourceHub und targetHub und andersrum
-            if(a.getHub1().equals(sourceHub)&& a.getHub2().equals(targetHub)||a.getHub1().equals(targetHub)&& a.getHub2().equals(sourceHub)){
+            if(a.isInPair(sourceHub, targetHub)){
                 this.gameServer.setGameLogicState(new AttackGameState(sourceHubId, targetHubId));
                 // Spielzustand auf AttackGameState mit den übergebenen Angreifer-Hub und Verteidiger Hub
+                return;
             }
         }
+        // Nicht benachbart -> Fehlermeldung ausgeben oder anzeigen
+        throw new IllegalArgumentException("Hubs sind nicht benachbart!");
     }
 }

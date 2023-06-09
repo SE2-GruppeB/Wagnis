5ca619770cf6d915c8c0d9d787b636668f6a0559
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2023-06-09 18:05:34 +0200

    Added a  Command Class
    Waiting for implementation

---
+++ app/src/main/java/at/aau/wagnis/server/communication/command/UseCardsCommand.java
@@ -0,0 +1,22 @@
+package at.aau.wagnis.server.communication.command;
+
+import androidx.annotation.NonNull;
+
+import at.aau.wagnis.gamestate.GameLogicState;
+
+public class UseCardsCommand implements ClientOriginatedServerCommand{
+    @Override
+    public int getClientId() {
+        return 0;
+    }
+
+    @Override
+    public void setClientId(int clientId) {
+
+    }
+
+    @Override
+    public void execute(@NonNull GameLogicState gameLogicState) {
+
+    }
+}

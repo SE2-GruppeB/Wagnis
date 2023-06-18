6059c2327715a332ab1a2521b6ff20c1c96eb183
martinpirker <126809496+martinpirker@users.noreply.github.com>
2023-06-18 19:10:51 +0200

    Dokumentation der EndGameState Klasse

--- app/src/main/java/at/aau/wagnis/gamestate/EndGameState.java
+++ app/src/main/java/at/aau/wagnis/gamestate/EndGameState.java
@@ -1,18 +1,25 @@
 package at.aau.wagnis.gamestate;
 
+/**
+ * Der Zustand des Spiellogik in welchem das Spiel beendet ist.
+ */
 public class EndGameState extends GameLogicState {
 
-
-    public EndGameState(){
+    /**
+     * Erzeugt eine neue Instanz des EndGameState.
+     */
+    public EndGameState() {
         super();
-
     }
 
-    //Todo
+    /**
+     * Beendet das Spiel, indem der GameServer geschlossen wird.
+     */
     @Override
     public void end() {
         //gameServer.broadcastCommand(null);  // send a command to all players to close the connection
 
-        gameServer.close(); // close the gameserver
+        gameServer.close();
+        // Schließt den GameServer
     }
 }
